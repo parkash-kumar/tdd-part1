8c5b41a2bbecbd72ddd791eb9d23738bd4314b32
parkash-kumar <parkash.kumar@globalnorthstar.com>
2015-01-01 12:19:35 +0500

    Sixth Chapter

--- src/Franc.java
+++ src/Franc.java
@@ -1,7 +1,5 @@
 
-public class Franc {
-
-	private int amount;
+public class Franc extends Money {
 	
 	Franc(int amount) {
 		this.amount = amount;
@@ -10,10 +8,4 @@ public class Franc {
 	Franc times(int multiplier) {
 		return new Franc(amount * multiplier);
 	}
-	
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return amount == franc.amount;
-	}
-
 }
